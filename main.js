5766d7a731059e23937a1898c5e16527355929b0
Ana Clara <anaclaraborbaa@gmail.com>
2026-02-14 03:24:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ window.onload = () => {
     setTimeout(() => {
         document.body.classList.remove("not-loaded");
         document.body.classList.add("loaded");
-    }, 1000);
+    }, 500);
 
-    // Adiciona posição aleatória para vagalumes no Safari
-    const fireflies = document.querySelectorAll('.firefly');
-    fireflies.forEach(firefly => {
+    for (let i = 0; i < 15; i++) {
+        const firefly = document.createElement('div');
+        firefly.className = 'firefly';
         firefly.style.left = Math.random() * 100 + 'vw';
         firefly.style.top = Math.random() * 100 + 'vh';
         firefly.style.animationDelay = Math.random() * 5 + 's';
-    });
+        document.body.appendChild(firefly);
+    }
 };
